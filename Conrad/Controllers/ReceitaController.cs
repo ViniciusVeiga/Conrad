7a37a5e0030a2789f9e734dc19a98c02effbdf1b
Vinicius Veiga <vinicius.veiga@vm2.com.br>
2018-11-27 16:41:25 -0200

    Terminado

---
+++ Conrad/Controllers/ReceitaController.cs
@@ -0,0 +1,53 @@
+﻿using Conrad.Model;
+using System;
+using System.Web.Mvc;
+
+namespace Conrad.Controllers
+{
+    public class ReceitaController : Controller
+    {
+        #region Index
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Item
+
+        public ActionResult Item(decimal? id)
+        {
+            MLReceita model = new MLReceita();
+
+            if(id == 1)
+            {
+                ViewBag.Nome = "Maria Joaquina";
+                model = new MLReceita
+                {
+                    Codigo = 1,
+                    CreditosUso = 3,
+                    CreditoUsados = 2,
+                    DataEmissao = Convert.ToDateTime("23/02/2017"),
+                    Validade = Convert.ToDateTime("22/02/2020")              
+                };
+            }
+
+            return View(model);
+        }
+
+        #region Salvar
+
+        public ActionResult Salvar(MLReceita model)
+        {
+            // Salvar usuário aqui
+
+            return View("Index");
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
